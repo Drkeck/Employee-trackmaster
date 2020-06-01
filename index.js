e9f8211a874831c3d3e2c7fb3f9a1bbd19339c48
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2020-05-31 20:04:16 -0700

    added sql calls for viewing departments, roles, and employee's.

--- index.js
+++ index.js
@@ -1,71 +1,100 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
-const {viewDepartments, addDepartments} = require('./utils/department');
+const db = require('./db/connection');
+const s = require('./utils/');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'TrotskY1@4',
-    database: 'employee_managerDB'
-});
-
-connection.connect(err => {
+db.connect(err => {
     if (err) throw err;
-    console.log('connected as id ' + connection.threadId);
-    console.log("welcome to employee taskmaster.")
-    initialQuery();
+    console.log('connected as id ' + db.threadId);
+    console.log("welcome to employee taskmaster.");
+
+    const sql = `SELECT first_name, last_name, manager, title, salary, department_name
+    FROM employee
+    JOIN roles ON employee.roles_id = roles.id
+    JOIN department ON roles.department_id = department.id;`
+
+    initializeList(sql)
+
 });
 
-initialQuery = () => {
-    connection.query(
-        `SELECT first_name, last_name, manager, title, salary, department_name
-        FROM employee
-        JOIN roles ON employee.roles_id = roles.id
-        JOIN department ON roles.department_id = department.id;`, function(err, res) {
+initializeList = (list) => {
+    db.query(list, function (err, res) {
             if (err) throw err;
 
             console.table(res);
-
-            inquirer.prompt([
-                {
-                    type: "list",
-                    name: "initialChoices",
-                    message: "What brings you here with us today?",
-                    choices: [
-                        "View all departments",
-                        "View all roles",
-                        "View all employees",
-                        "Add new department",
-                        "Add new roles",
-                        "Add new employee",
-                        "update an employee's roll"
-                    ]
-                }
-            ])
-            .then((answer) => {
-                if (answer === "View all departments") {
-                    viewDepartments();
-                }
-                if (answer === "View all roles") {
-                    //function
-                }
-                if (answer === "view all employees") {
-                    //function
-                }
-                if (answer === "Add new department") {
-                    //f
-                }
-                if (answer === "Add new roles") {
-                    //f
-                }
-                if (answer === "Add new employee") {
-                    //f
-                }
-                if (answer === "update employee's roll") {
-                    //s
-                }
-            })
-        });
+            initialQuery();
+        }
+    )
 }
+
+function initialQuery() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choices",
+            message: "What brings you here with us today?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add new department",
+                "Add new roles",
+                "Add new employee",
+                "update an employee's roll",
+                "Exit"
+            ]
+        }
+    ])
+        .then((answer) => {
+            const { choices } = answer;
+            
+            if (choices === "View all departments") {
+
+                console.log('this is what i could find on our departments:');
+
+                let sql = `SELECT * FROM department`;
+
+                initializeList(sql);
+            }
+            if (choices === "View all roles") {
+
+                console.log('Here is our list of employee roles:');
+
+                let sql = `SELECT title, salary, department_name 
+                            FROM roles
+                            JOIN department 
+                            ON roles.department_id = department.id`;
+
+                initializeList(sql);            
+            }
+            if (choices === "View all employees") {
+
+                console.log('I have the list of all our recorded employees');
+
+                let sql = `SELECT first_name, last_name, manager, title, salary, department_name
+                FROM employee
+                JOIN roles ON employee.roles_id = roles.id
+                JOIN department ON roles.department_id = department.id`;
+
+                initializeList(sql);
+            }
+            if (choices === "Add new department") {
+                //f
+            }
+            if (choices === "Add new roles") {
+                //f
+            }
+            if (choices === "Add new employee") {
+                //f
+            }
+            if (choices === "update employee's roll") {
+                //s
+            }
+            if (choices === "Exit") {
+                console.log('goodbye');
+                db.end();
+            }
+        })
+
+};
