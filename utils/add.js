41ec8df562401735a69624e7df5dbdcf98b1b468
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2020-05-31 20:02:57 -0700

    added sql calls for viewing departments, roles, and employee's.

---
+++ utils/add.js
@@ -0,0 +1,23 @@
+const db = require('../db/connection');
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const cTable = require('console.table');
+
+const addDepartment = () => {
+
+};
+
+const addRole = () => {
+    
+};
+
+const addEmployee = () => {
+
+};
+
+
+module.exports = {
+    addDepartment,
+    addRole,
+    addEmployee
+}
